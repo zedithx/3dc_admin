83b9b3df1b2d66b363de585da3287e26f76f2d28
Yang Si Jun <aersijun@gmail.com>
2023-12-26 19:00:51 +0800

    made change to build.sh

--- dc_admin/build.sh
+++ dc_admin/build.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
-cd dc_admin
 
-pip install -r requirements.txt
+cd dc_admin
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+pip install -r requirements.txt
+
