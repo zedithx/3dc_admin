942af96731cfc226abed2d1c33f9090325051551
Yang Si Jun <aersijun@gmail.com>
2023-12-26 19:46:16 +0800

    fix urls to collect static:<

--- dc_admin/backend/urls.py
+++ dc_admin/backend/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', admin.site.urls),
     path("api/", include('api_calls.urls'))
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
