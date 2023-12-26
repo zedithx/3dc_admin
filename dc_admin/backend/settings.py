fdf99489e61e84d3c07f66668979e23fd42ee5a4
Yang Si Jun <aersijun@gmail.com>
2023-12-27 01:53:43 +0800

    fixin gofr the dk how many time

--- dc_admin/backend/settings.py
+++ dc_admin/backend/settings.py
@@ -121,8 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = 'staticfiles'
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 print(STATIC_ROOT)
 
 # Default primary key field type
