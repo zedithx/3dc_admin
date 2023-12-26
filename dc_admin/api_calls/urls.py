7ce8a3c45bc571542e4a49a35c7e274a85a743fc
Yang Si Jun <aersijun@gmail.com>
2023-12-27 02:13:47 +0800

    fix api call

--- dc_admin/api_calls/urls.py
+++ dc_admin/api_calls/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    #Define api paths ehre
+    #Define api paths here
     path('hello-world/', views.hello_world, name='hello_world'),
 ]
--- dc_admin/backend/settings.py
+++ dc_admin/backend/settings.py
@@ -123,7 +123,6 @@ USE_TZ = True
 STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 # STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-print(STATIC_ROOT)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
